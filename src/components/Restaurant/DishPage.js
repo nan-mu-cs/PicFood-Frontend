19b5de67d83c6e596e97e9b27f8e20c2204cae05
Chaofeng <Z.Chaofeng94@gmail.com>
2018-03-23 22:57:28 -0400

    add new functions in network controller

---
+++ src/components/Restaurant/DishPage.js
@@ -0,0 +1,111 @@
+/**
+ * Created by jin on 19/03/2018.
+ */
+
+import React, {Component} from 'react';
+import {
+  Container, Header, Content, Button, Text, Icon, ListItem, Left, Body,
+  Card, CardItem, List, Title, Right
+} from 'native-base';
+import {StyleSheet, ScrollView, Image} from 'react-native';
+import {connect} from 'react-redux';
+import Dishes from "./Dishes";
+import {Col, Row, Grid} from "react-native-easy-grid";
+import Footer from "./Footer"
+import StarRating from 'react-native-star-rating';
+
+class DishPage extends Component {
+  constructor(props, context) {
+    super(props);
+    this.state = {};
+
+    this.handleClickBack = this.handleClickBack.bind(this);
+  }
+
+  handleClickBack() {
+    this.props.history.goBack();
+  }
+
+  render() {
+    let dish = this.props.restaurant.dishes.map((item) => {
+      let dishes;
+
+      dishes = <Dishes data={item}/>;
+      return (
+        <ListItem key={item.id} style={styles.listItem}>
+          {dishes}
+        </ListItem>
+      );
+    });
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent onPress={this.handleClickBack}>
+              <Icon name='arrow-back'/>
+            </Button>
+          </Left>
+          <Body>
+          <Title>Restaurant</Title>
+          </Body>
+          <Right/>
+        </Header>
+        <Grid>
+          <Row size={9}>
+            <Col>
+              <ScrollView>
+                <Card>
+                  <CardItem>
+                    <Left>
+                      <Body>
+                      <Text>{this.props.restaurant.name}</Text>
+                      <StarRating
+                        disabled={true}
+                        maxStars={5}
+                        rating={this.props.restaurant.rate}
+                        containerStyle={{marginTop: 10, alignSelf: "center"}}
+                        fullStarColor={"#f5af4b"}
+                        emptyStarColor={"#f5af4b"}
+                        halfStarEnabled
+                        starSize={25}
+                      />
+                      <Text note>{this.props.restaurant.location}</Text>
+                      <Text note>{this.props.restaurant.address}</Text>
+                      </Body>
+                    </Left>
+                  </CardItem>
+                  <CardItem cardBody>
+                    <Image source={{uri: this.props.restaurant.avatar}} style={{height: 200, width: null, flex: 1}}/>
+                  </CardItem>
+
+                  <CardItem>
+                    <ScrollView>
+                      <List>
+                        {dish}
+                      </List>
+                    </ScrollView>
+                  </CardItem>
+
+                </Card>
+              </ScrollView>
+            </Col>
+          </Row>
+        </Grid>
+        <Footer/>
+      </Container>
+    )
+  }
+}
+
+const styles = StyleSheet.create({});
+
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    restaurant: state.restaurant
+  }
+};
+
+export default connect(
+  mapStateToProps
+)(DishPage);
--- src/network/index.js
+++ src/network/index.js
@@ -3,19 +3,19 @@ const HOST = 'http://172.26.62.62:8080';
 export default {
 
   account: {
-    login(body) {
-      return fetch(`/login`, verb('post', body)).then(handleResponse);
+    login(email, password) {
+      return fetch(`/login`, verb('post', {email, password})).then(handleResponse);
     },
 
-    register(body) {
-      return fetch(`/register`, verb('post', body)).then(handleResponse);
+    register(email, password) {
+      return fetch(`/register`, verb('post', email, password)).then(handleResponse);
     },
 
     getUserAccount() {
       return fetch(HOST + '/api/users/me', verb('get')).then(handleResponse);
     },
 
-    postUserAccount(body) {
+    postUserAccount(body) { // avatar, bio, email, name, password (all optional)
       return fetch(HOST + '/api/users/me', verb('post', body)).then(handleResponse);
     },
   },
@@ -33,12 +33,20 @@ export default {
       return fetch(HOST + `/api/post/${id}`, verb('get')).then(handleResponse);
     },
 
-    deletePost(id) {
-      return fetch(HOST + `/delete/post`, verb('post', {id})).then(handleResponse);
+    deletePost(postId) {
+      return fetch(HOST + `/delete/post`, verb('post', {postId})).then(handleResponse);
     },
 
-    postPost(body) {
+    addPost(body) { // restaurantId, dishName, rate, category, content, imageId (optional)
       return fetch(HOST + `/post`, verb('post', body)).then(handleResponse);
+    },
+
+    upvotePost(postId) {
+      return fetch(HOST + `/upvote`, verb('post', {postId})).then(handleResponse);
+    },
+
+    deleteUpvoteOfPost(postId) {
+      return fetch(HOST + `/delete/upvote`, verb('post', {postId})).then(handleResponse);
     },
 
     getMyFollowers() {
@@ -57,12 +65,12 @@ export default {
       return fetch(HOST + `/api/followings/${id}`, verb('get')).then(handleResponse);
     },
 
-    postFollow(id) {
-      return fetch(HOST + `/api/follow`, verb('post', {id})).then(handleResponse);
+    followUserById(id) {
+      return fetch(HOST + `/api/follow/?id=${id}`, verb('post', {})).then(handleResponse);
     },
 
-    postUnfollow(id) {
-      return fetch(HOST + `/api/unfollow`, verb('post', {id})).then(handleResponse);
+    unfollowUserById(id) {
+      return fetch(HOST + `/api/unfollow/?id=${id}`, verb('post', {})).then(handleResponse);
     },
   },
 
@@ -77,6 +85,10 @@ export default {
 
     getRestaurantsByLocation(location) {
       return fetch(HOST + `/api/restaurants/${location}`, verb('get')).then(handleResponse);
+    },
+
+    searchRestaurants(keyword) {
+      return fetch(HOST + `/search/restaurants?keyword=${keyword}`, verb('get')).then(handleResponse);
     }
 
   },
@@ -94,26 +106,30 @@ export default {
       return fetch(HOST + `/api/dishes/${id}/post`, verb('get')).then(handleResponse);
     },
 
-    postPhoto(body) {
+    postPhoto(body) { // {FormData} file
       return fetch(HOST + `/storage/uploadFile`, verb('post', body)).then(handleResponse);
     },
 
     deletePhoto(fileUrl) {
       return fetch(HOST + `/storage/deleteFile`, verb('delete', {fileUrl})).then(handleResponse);
     },
+
+    searchDishes(keyword) {
+      return fetch(HOST + `/search/dishes?keyword=${keyword}`, verb('get')).then(handleResponse);
+    }
   },
 
   comment: {
-    getCommentById(id) {
-      return fetch(HOST + `/api/comments/${id}`, verb('get')).then(handleResponse);
+    getCommentsOfPost(postId) {
+      return fetch(HOST + `/api/comments/${postId}`, verb('get')).then(handleResponse);
     },
 
-    postComment(body) {
-      return fetch(HOST + '/comment', verb('post', body)).then(handleResponse);
+    postComment(postId, content) {
+      return fetch(HOST + '/comment', verb('post', {postId, content})).then(handleResponse);
     },
 
-    deleteComment(id) {
-      return fetch(HOST + '/delete/comment', verb('post', {id})).then(handleResponse);
+    deleteComment(commentId) {
+      return fetch(HOST + '/delete/comment', verb('post', {commentId})).then(handleResponse);
     }
   }
 }
