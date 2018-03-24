4d510115060570a4e9443418e89b548f6b3c519c
Chaofeng <Z.Chaofeng94@gmail.com>
2018-03-24 17:34:40 -0400

    small bug fix

--- src/network/index.js
+++ src/network/index.js
@@ -165,8 +165,6 @@ function verb(method, body) {
   let data ={
     method: method,
     headers: {
-      'Content-Type': 'application/json; charset=UTF-8',
-      'Authorization': store.getState().token
       'Content-Type':'application/json; charset=UTF-8',
       'Authorization': `${store.getState().token}`
     },
